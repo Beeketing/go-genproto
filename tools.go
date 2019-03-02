bafa213bc890cfee7e06a43581ea5d4986777ec3
hauphan <hauphan@beeketing.net>
2019-03-02 13:49:38 +0700

    custom package

--- tools.go
+++ tools.go
@@ -24,7 +24,7 @@
 package genproto
 
 import (
-	_ "github.com/golang/protobuf/protoc-gen-go"
+	_ "github.com/Beeketing/protobuf/protoc-gen-go"
 	_ "golang.org/x/exp/cmd/apidiff"
 	_ "golang.org/x/lint/golint"
 	_ "golang.org/x/tools/cmd/goimports"
